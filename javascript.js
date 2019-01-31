479b5ecc0af7eeb346a6fbc12b717360926a8cb0
DingwenSun <dsun01@risd.edu>
2019-01-30 19:02:42 -0500

    version1.6.1

--- javascript.js
+++ javascript.js
@@ -224,12 +224,10 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-// We listen to the resize event
-window.addEventListener('resize', () => {
+if($(window).width() >= 1024){
+  // We listen to the resize event
+  window.addEventListener('resize', () => {
   // We execute the same script as before
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
-$(window).on('scroll', _.debounce(setProperty, 500));
-$(window).on('resize', _.debounce(setProperty, 500));
+})};
