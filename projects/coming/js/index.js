8c3320e8b4240295912f06f22d401a3f5f6d7fba
DingwenSun <dsun01@risd.edu>
2019-02-09 13:29:37 -0500

    coming overflow vh

--- projects/coming/js/index.js
+++ projects/coming/js/index.js
@@ -43,3 +43,17 @@ particleground(document.getElementById('particles-background'), {
   parallaxMultiplier: 20, // Lower the number is more extreme parallax
   particleRadius: 2, // Dot size
 });
+
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+if($(window).width() >= 1024){
+  // We listen to the resize event
+  window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+})};
