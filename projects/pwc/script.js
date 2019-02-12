f508aac45b17a19e70e0e69ad43734b79ac7a954
DingwenSun <dsun01@risd.edu>
2019-02-12 16:47:43 -0500

    subpage update to pwc

---
+++ projects/pwc/script.js
@@ -0,0 +1,35 @@
+(function($){
+  $(window).on("load",function(){
+    $("#content-5").mCustomScrollbar({
+      axis:"x",
+      theme:"dark-thin",
+      scrollbarPosition: "inside",
+      autoExpandScrollbar:false,
+      advanced:{autoExpandHorizontalScroll:true}
+    });
+  });
+})(jQuery);
+
+
+
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    var y = document.getElementById("menu");
+    if (x.className.indexOf("show") == -1) {
+        x.className += " show";
+        y.className += " open"
+    } else {
+        x.className = x.className.replace(" show", "");
+        y.className = y.className.replace(" open", "");
+    }
+}
